33e3dcc2616d5b81f42e241cb25d583393168d91
JadirLevario152 <staffs-habbo.es@hotmail.com>
2025-11-10 21:32:09 -0500

    ultimo

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let val1;
+let val2;
+let val3;
+
+function calculateMont() {
+    val1 = parseFloat(document.getElementById('grocery1').value);
+    val2 = parseFloat(document.getElementById('grocery2').value);
+    val3 = parseFloat(document.getElementById('grocery3').value);
+   
+    let total = val1 + val2 + val3;
+    document.getElementById('resultMont').innerText = `El monto total es: ${total}`;
+}
