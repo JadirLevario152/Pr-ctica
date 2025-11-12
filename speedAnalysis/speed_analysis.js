1ca22b2ca2f6dffd14c40c3c9630777938fd3c93
JadirLevario152 <staffs-habbo.es@hotmail.com>
2025-11-12 00:24:01 -0500

    actualizado

---
+++ speedAnalysis/speed_analysis.js
@@ -0,0 +1,50 @@
+let testText = "El rápido zorro marrón salta sobre el perro perezoso.";
+let startTime, endTime;
+
+function startTest() {
+    // Establecer el texto de prueba
+    document.getElementById("inputText").value = testText;
+    
+    // Reiniciar la entrada y salida del usuario
+    let userInput = document.getElementById("userInput");
+    userInput.value = "";
+    userInput.readOnly = false;
+    userInput.focus();
+    
+    document.getElementById("output").innerHTML = "";
+    
+    // Iniciar el temporizador
+    startTime = new Date().getTime();
+    }
+
+    function endTest() {
+        endTime = new Date().getTime();
+
+        // Deshabilitar la entrada del usuario
+        document.getElementById("userInput").readOnly = true;
+
+        // Calcular el tiempo transcurrido y las palabras por minuto (WPM)
+        var timeElapsed = (endTime - startTime) / 1000; // en segundos
+        var userTypedText = document.getElementById("userInput").value;
+
+        // Dividir el texto usando regex para contar las palabras correctamente
+        var typedWords = userTypedText.split(/\s+/).filter(function (word) {
+            return word !== "";
+        }).length;
+
+        var wpm = 0; // Valor por defecto
+
+        var wordsLenght = userTypedText.length;
+
+        if (timeElapsed !== 0 && !isNaN(typedWords)) {
+            wpm = Math.round((typedWords / timeElapsed) * 60);
+        }
+
+        // Mostrar los resultados
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = "<h2>Resultados de la Prueba de Escritura:</h2>" +
+            "<p>Tamaño de la oración: " + wordsLenght + "</p>" +
+            "<p>Palabras Escritas: " + typedWords + "</p>" +
+            "<p>Tiempo Transcurrido: " + timeElapsed.toFixed(2) + " segundos</p>" +
+            "<p>Palabras Por Minuto (WPM): " + wpm + "</p>";
+        }
